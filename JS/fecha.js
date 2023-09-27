032611546f8c15511b8bbcf12f3de5262473d963
David Rodriguez Gallego <david.rodgal@educa.jcyl.es>
2023-09-27 20:08:53 +0200

    SUBIDA FINAL

---
+++ JS/fecha.js
@@ -0,0 +1,7 @@
+const fecha= new Date();
+const diaMes=fecha.getDate();
+const mes=(fecha.getMonth()+1).toString().padStart(2,'0');
+const anio=fecha.getFullYear();
+const fechaObtenida=`${diaMes}/${mes}/${anio}`;
+
+document.getElementById('fecha').textContent= fechaObtenida;
