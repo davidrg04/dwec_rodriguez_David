239fb34512d3af295b7e3c4f497ba2b553c62f22
David Rodriguez Gallego <david.rodgal@educa.jcyl.es>
2023-09-29 13:25:15 +0200

    Modificacion ejercicio 10

--- JS/fecha.js
+++ JS/fecha.js
@@ -3,5 +3,6 @@ const diaMes=fecha.getDate();
 const mes=(fecha.getMonth()+1).toString().padStart(2,'0');
 const anio=fecha.getFullYear();
 const fechaObtenida=`${diaMes}/${mes}/${anio}`;
-
-document.getElementById('fecha').textContent= fechaObtenida;
+// document.getElementById('fecha').textContent= `${diaMes}/${mes}/${anio}`;
+let element=document.getElementById('fecha');
+element.innerHTML=fechaObtenida;
--- UNIDADES/UT01/PRACTICAS/PR0210.js
+++ UNIDADES/UT01/PRACTICAS/PR0210.js
@@ -1,38 +1,53 @@
-let numero1=Number(prompt('Dime el primer número'));
-let numero2=Number(prompt('Dime el segundo número'));
-let numero3=Number(prompt('Dime el tercer número'));
-let numero4=Number(prompt('Dime el cuarto número'));
-let numero5=Number(prompt('Dime el quinto número'));
+// let numero1=Number(prompt('Dime el primer número'));
+// let numero2=Number(prompt('Dime el segundo número'));
+// let numero3=Number(prompt('Dime el tercer número'));
+// let numero4=Number(prompt('Dime el cuarto número'));
+// let numero5=Number(prompt('Dime el quinto número'));
 
-let mayor=numero1;
-let menor=numero1;
+// let mayor=numero1;
+// let menor=numero1;
 
-if (numero2<menor) {
-    menor=numero2;
-}
-if (numero3<menor){
-    menor=numero3;
-}
-if (numero4<menor) {
-    menor=numero4;
-}
-if (numero5<menor) {
-    menor=numero5;
-} 
+// if (numero2<menor) {
+//     menor=numero2;
+// }
+// if (numero3<menor){
+//     menor=numero3;
+// }
+// if (numero4<menor) {
+//     menor=numero4;
+// }
+// if (numero5<menor) {
+//     menor=numero5;
+// } 
 
-if (numero2>mayor) {
-    mayor=numero2;
-}
-if (numero3>mayor){
-    mayor=numero3;
-}
-if (numero4>mayor) {
-    mayor=numero4;
-}
-if (numero5>mayor) {
-    mayor=numero5;
-} 
+// if (numero2>mayor) {
+//     mayor=numero2;
+// }
+// if (numero3>mayor){
+//     mayor=numero3;
+// }
+// if (numero4>mayor) {
+//     mayor=numero4;
+// }
+// if (numero5>mayor) {
+//     mayor=numero5;
+// } 
 
-alert(`El mayor número es ${mayor} y el menor es ${menor}`);
+// alert(`El mayor número es ${mayor} y el menor es ${menor}`);
+let numero=Number(prompt(`Dime el numero 1`));
+let numeroMenor=numero;
+let numeroMayor=numero;
+
+for (let i = 2; i <=5; i++) {
+    let n1=Number(prompt(`Dime el numero ${i}`));
+    if (n1<numeroMenor) {
+        numeroMenor=n1;
+    }
+    if(n1>numeroMayor){
+        numeroMayor=n1;
+    }
+}
+
+alert(`El numero menor es ${numeroMenor} y el mayor es ${numeroMayor}`);
 
     
