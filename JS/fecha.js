10ff39b2cde4da29a536887a9d72c7ef206d8725
David Rodriguez Gallego <david.rodgal@educa.jcyl.es>
2023-10-03 19:52:42 +0200

    Publicando pagina 3 de octubre

--- JS/fecha.js
+++ JS/fecha.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     const fecha= new Date();
-    const diaMes=fecha.getDate();
+    const diaMes=(fecha.getDate().toString().padStart(2,'0'));
     const mes=(fecha.getMonth()+1).toString().padStart(2,'0');
     const anio=fecha.getFullYear();
     const fechaObtenida=`${diaMes}/${mes}/${anio}`;
