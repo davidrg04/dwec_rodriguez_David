54336c1736d45d39ab59d1ed60063124843a9063
David Rodriguez Gallego <david.rodgal@educa.jcyl.es>
2023-10-10 20:03:57 +0200

    Nuevas tareas

--- UNIDADES/UT03/PRACTICAS/PR0303.js
+++ UNIDADES/UT03/PRACTICAS/PR0303.js
@@ -1,21 +1,33 @@
 let usuario1 = {
-    nombre:"David",
-    edad : 19
+    edad: 19,
+    estudia : false,
+    trabaja: true,
+    nombre: "David"
 }
 let usuario2 = {
     nombre: "Pablo",
     ciudad: "Benavente",
-    apellido: "Rodriguez"
+    apellido: "Rodriguez",
+    pais: "España",
+    edad: "50"
 }
 
-
+let usuario3=intersectObjects(usuario1,usuario2);
+for (let key in usuario3) {
+    console.log(usuario3[key]);
+}
 
 
 function intersectObjects(usuario1, usuario2) {
     let usuario = {};
 
     for (let key in usuario1) {
-        
+        let llave=key;
+        for (let key2 in usuario2) {
+            if (key2==llave) {
+                usuario[key]= usuario1[key];
+            }
+        }
     }
 
 
---
+++ UNIDADES/UT03/PRACTICAS/PR0304.js
@@ -0,0 +1,47 @@
+let usuario1 = {
+    nombre: "David",
+    nick: "DRG"
+ }
+ let usuario2 = {
+    nombre: "Juan",
+    nick: "uop"
+ }
+
+let resultado=containsObject(usuario1,usuario2);
+alert(resultado);
+
+ function containsObject(usuario1, usuario2) {
+    let esta=false
+    let array1=[];
+    let array2=[];
+
+    for (let key in usuario1) {
+        array1.push(key);
+    }
+    for (let key2 in usuario2) {
+        array2.push(key2);
+    }
+    
+
+    if (!(array1.length === array2.length)) {
+        return false;
+    }else{
+        for (let i = 0; i < array1.length; i++) {
+            for (let j = 0; j < array2.length; j++) {
+                if (array1[i]===array2[j]) {
+                    esta=true;
+                    break;
+                }else{
+                    esta=false;
+                }
+                
+            }
+            if (esta==false) {
+                break;
+            }
+            
+        }
+    }
+
+    return (esta==true) ? true : false;
+ }
