9350e2753eec6985afaa56749023cd4b85e406ce
David Rodriguez Gallego <david.rodgal@educa.jcyl.es>
2023-10-11 14:14:48 +0200

    nuevo

--- UNIDADES/UT03/PRACTICAS/PR0303.js
+++ UNIDADES/UT03/PRACTICAS/PR0303.js
@@ -22,9 +22,8 @@ function intersectObjects(usuario1, usuario2) {
     let usuario = {};
 
     for (let key in usuario1) {
-        let llave=key;
         for (let key2 in usuario2) {
-            if (key2==llave) {
+            if (key2===key) {
                 usuario[key]= usuario1[key];
             }
         }
--- UNIDADES/UT03/PRACTICAS/PR0304.js
+++ UNIDADES/UT03/PRACTICAS/PR0304.js
@@ -1,47 +1,70 @@
-let usuario1 = {
-    nombre: "David",
-    nick: "DRG"
- }
- let usuario2 = {
-    nombre: "Juan",
-    nick: "uop"
- }
+// let usuario1 = {
+//     nombre: "David",
+//     nick: "DRG"
+//  }
+//  let usuario2 = {
+//     nombre: "Juan",
+//     nick: "uop"
+//  }
 
-let resultado=containsObject(usuario1,usuario2);
-alert(resultado);
+// let resultado=containsObject(usuario1,usuario2);
+// alert(resultado);
 
- function containsObject(usuario1, usuario2) {
-    let esta=false
-    let array1=[];
-    let array2=[];
+//  function containsObject(usuario1, usuario2) {
+//     let esta=false
+//     let array1=[];
+//     let array2=[];
 
-    for (let key in usuario1) {
-        array1.push(key);
-    }
-    for (let key2 in usuario2) {
-        array2.push(key2);
-    }
+//     for (let key in usuario1) {
+//         array1.push(key);
+//     }
+//     for (let key2 in usuario2) {
+//         array2.push(key2);
+//     }
     
 
-    if (!(array1.length === array2.length)) {
-        return false;
-    }else{
-        for (let i = 0; i < array1.length; i++) {
-            for (let j = 0; j < array2.length; j++) {
-                if (array1[i]===array2[j]) {
-                    esta=true;
-                    break;
-                }else{
-                    esta=false;
-                }
+//     if (!(array1.length === array2.length)) {
+//         return false;
+//     }else{
+//         for (let i = 0; i < array1.length; i++) {
+//             for (let j = 0; j < array2.length; j++) {
+//                 if (array1[i]===array2[j]) {
+//                     esta=true;
+//                     break;
+//                 }else{
+//                     esta=false;
+//                 }
                 
-            }
-            if (esta==false) {
-                break;
-            }
+//             }
+//             if (esta==false) {
+//                 break;
+//             }
             
+//         }
+//     }
+
+//     return (esta==true) ? true : false;
+//  }
+
+let usuario1 = {
+    nombre : "David",
+    nick : "drg"
+}
+let usuario2 = {
+    nombre : "David",
+    nick : "drg",
+    ciudad : "benavente"
+}
+
+function containsObject(usuario1,usuario2) {
+    
+    for (let key in usuario1) {
+        if (!(key in usuario2)) {
+            return false;
         }
     }
 
-    return (esta==true) ? true : false;
- }
+    return true;
+}
+
+
